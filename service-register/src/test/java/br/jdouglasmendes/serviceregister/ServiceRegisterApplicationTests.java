cf1a1cf412ebfdbfd107478dfbbd3a2dacc78c8a
Douglas Mendes <douglasaleixomendes@gmail.com>
2022-09-05 23:21:50 -0300

    add project

---
+++ service-register/src/test/java/br/jdouglasmendes/serviceregister/ServiceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jdouglasmendes.serviceregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
